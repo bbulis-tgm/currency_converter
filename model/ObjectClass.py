c4d95056fd5cea6003653cb8f011f4cec4eff54f
Benjamin Bulis <beni1999@gmx.at>
2020-11-11 21:59:30 +0100

    Added Methode CalcResult for returning results in html format

--- model/ObjectClass.py
+++ model/ObjectClass.py
@@ -10,3 +10,23 @@ class CurrencyResult:
 
     def __str__(self):
         return "<li><b>{:.2f} {}</b> (Kurs: {:f}</li>".format(self._value, self._name, self._wechselkurs)
+
+
+class ClacResult:
+
+    def __init__(self, _value: float, _from: str, _to: List[CurrencyResult], _date: str):
+        self._value = _value
+        self._from = _from
+        self._to = _to
+        self._date = _date
+
+    def __str__(self):
+        ret = ""
+        ret += "<b>{:.2f} {}</b> entsprechen<br>".format(self._value, self._from)
+        ret += "<ul>"
+        for s in self._to:
+            ret += str(s)
+        ret += "</ul>"
+        ret += "Stand: {}".format(self._date)
+
+        return ret
