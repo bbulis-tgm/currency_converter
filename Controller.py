2f1e753f2630f28327c6fc4f8f1363f622952e28
Benjamin Bulis <beni1999@gmx.at>
2020-11-11 22:26:45 +0100

    Added Controller Class to Project

---
+++ Controller.py
@@ -0,0 +1,8 @@
+import sys
+from view.MyView import Ui_MainWindow
+from PyQt5 import QtWidgets
+from model.LiveStrategy import LiveStrategy
+
+
+class Controller(QtWidgets.QMainWindow, Ui_MainWindow):
+    pass
