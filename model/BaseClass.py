c7d62982f77cf971d8e35d3c0957eb95a2a116b9
Benjamin Bulis <beni1999@gmx.at>
2020-11-11 16:40:25 +0100

    Finished BaseClass with abstractmethode for calc currencies

---
+++ model/BaseClass.py
@@ -0,0 +1,18 @@
+from abc import ABC
+from abc import abstractmethod
+from typing import List
+
+class BaseClass(ABC):
+
+    @abstractmethod
+    def calculate(self, _value: float, _from: str, _to: List[str]):
+        """
+        Methode, welche die Umrechnung durchführt
+
+        :param _value: Der Betrag welcher umgerechnet werden soll
+        :param _from: Die Währung für den Betrag welcher der Methode übergeben wird
+        :param _to: Liste aller Währungen in welche der Betrag umgerechnet wird
+        :return: Dict aller Werte und Währungen welche der Methode anfangs übergeben wurden
+        :raise: Error wenn bei der Umrechnung ein Fehler aufgetreten ist
+        """
+        pass
